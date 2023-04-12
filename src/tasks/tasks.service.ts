698ff9ee376ea0a4401745c1bd57cbdc74daa54b
ignaciozhetta <zagu4988@gmail.com>
2023-04-12 11:07:02 -0300

    updated and deleted added

--- src/tasks/tasks.service.ts
+++ src/tasks/tasks.service.ts
@@ -1,6 +1,7 @@
 import { Injectable } from '@nestjs/common';
 import { Task, TaskStatus } from './task.entity';
 import { v4 } from 'uuid';
+import { UpdateTaskDto } from './dto/task.dto';
 
 @Injectable()
 export class TasksService {
@@ -27,6 +28,19 @@ export class TasksService {
     this.tasks.push(task);
     return task;
   }
-  updateTask() {}
-  deleteTask() {}
+
+  getTaskById(id: string): Task {
+    return this.tasks.find((task) => task.id === id);
+  }
+
+  updateTask(id: string, updatedFields: UpdateTaskDto): Task {
+    const task = this.getTaskById(id);
+    const newTask = Object.assign(task, updatedFields);
+    this.tasks = this.tasks.map((task) => (task.id === id ? newTask : task));
+    return newTask;
+  }
+
+  deleteTask(id: string) {
+    this.tasks = this.tasks.filter((task) => task.id !== id);
+  }
 }
